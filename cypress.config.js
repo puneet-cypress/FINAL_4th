fc958fec4059cd2d71185a99dd1c0d18b76e564e
Puneet Aneja <puneetaneja@Puneets-Air.attlocal.net>
2024-07-09 01:20:13 -0700

    added test cases for loggedout

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,20 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: "https://opensource-demo.orangehrmlive.com",
+    supportFile: "cypress/support/e2e.js",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
 });
+
+//baseUrl: 'https://opensource-demo.orangehrmlive.com',
+
+//module.exports = { //execute hota hain. har ek test mein call hoga...same with reporting s¥stem. this is to set configruation..
+//   e2e: {
+//     baseUrl: "https://opensource-demo.orangehrmlive.com",
+//     supportFile: "cypress/support/e2e.js"
+
+//   }
+// };
--- cypress/e2e/tests/logintest.cy.js
+++ cypress/e2e/tests/logintest.cy.js
@@ -5,30 +5,53 @@
 import LoginPage from "../pages/loginpage.cy.js";
 
 describe('Login Test', () => {
+    let testdata;
 
     const loginpage = new LoginPage();
 
 beforeEach(() => {
-        cy.visit('https://opensource-demo.orangehrmlive.com');
+    cy.fixture('credential_file').then( (testdata_original)=> {
+        testdata = testdata_original;
+      });
+        cy.visit('/');
+        
+       
     });
 
 
     it('Login Test', () => {
+        //cy.log(testdata.username); this is how you can log the data:::
+
         loginpage.loginbuttonexist().contains('Login').should('be.visible');
+   
     })
 
-    it('Enter Username', () => {
-        loginpage.enterUsername("Admin");
-        loginpage.enterPassword("admin123");
+    it('valid credentials', () => {
+        loginpage.enterUsername(testdata.username);
+        loginpage.enterPassword(testdata.password);
         loginpage.clickLogin();
         cy.url().should('include', '/web/index.php/dashboard/index');
+        loginpage.usr_logged_in();
+        cy.wait(1000);
+        loginpage.user_logout();
+
     })
 
     it('Enter wrong email and password', () => {
-        loginpage.enterUsername("Admin123");
-        loginpage.enterPassword("wrongpassword");
+        loginpage.enterUsername(testdata.invalid_username);
+        loginpage.enterPassword(testdata.invalid_password);
         loginpage.clickLogin();
         cy.url().should('not.include' , '/web/index.php/dashboard/index');
         loginpage.getErrorMessage("Invalid credentials");
        })
+
+       it('Enter empty email and password', () => {
+        loginpage.clickLogin();
+        cy.contains('Required').should('be.visible');
+        cy.url().should('not.include' , '/web/index.php/dashboard/index');
+       })
+
+
 });
+
+
