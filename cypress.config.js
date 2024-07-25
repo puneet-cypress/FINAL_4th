b10ea6b376615081b67bb0198565c02c6e65e6fe
Puneet Aneja <puneetaneja@Puneets-Air.attlocal.net>
2024-07-24 19:18:37 -0700

    add more test with valid.logout

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "https://opensource-demo.orangehrmlive.com",
+    baseUrl: "https://ecommerce-playground.lambdatest.io/",
+    //baseUrl: "https://ecommerce-playground.lambdatest.io/index.php?route=account/login",
     supportFile: "cypress/support/e2e.js",
     setupNodeEvents(on, config) {
       // implement node event listeners here
--- cypress/e2e/tests/logintest.cy.js
+++ cypress/e2e/tests/logintest.cy.js
@@ -1,57 +1,63 @@
 // <reference types="cypress" />
 
-
-//import LoginPage from "../pages/loginpage.cy.js";
 import LoginPage from "../pages/loginpage.cy.js";
 
-describe('Login Test', () => {
-    let testdata;
+describe("Login Test", () => {
+  let testdata;
 
-    const loginpage = new LoginPage();
+  const loginpage = new LoginPage();
 
-beforeEach(() => {
-    cy.fixture('credential_file').then( (testdata_original)=> {
-        testdata = testdata_original;
-      });
-        cy.visit('/');
-        
-       
+  beforeEach(() => {
+    cy.fixture("credential_file").then((testdata_original) => {
+      testdata = testdata_original;
     });
+    cy.visit("/"); //we ket it black because we have already defined the url in the loginpage.cy.js
+    cy.get('[data-toggle="dropdown"]').eq(3).trigger("mouseover");
+    cy.contains("Login").click();
+  });
 
-
-    it('Login Test', () => {
-        //cy.log(testdata.username); this is how you can log the data:::
-
-        loginpage.loginbuttonexist().contains('Login').should('be.visible');
-   
-    })
-
-    it('valid credentials', () => {
-        loginpage.enterUsername(testdata.username);
-        loginpage.enterPassword(testdata.password);
-        loginpage.clickLogin();
-        cy.url().should('include', '/web/index.php/dashboard/index');
-        loginpage.usr_logged_in();
-        cy.wait(1000);
-        loginpage.user_logout();
-
-    })
-
-    it('Enter wrong email and password', () => {
-        loginpage.enterUsername(testdata.invalid_username);
-        loginpage.enterPassword(testdata.invalid_password);
-        loginpage.clickLogin();
-        cy.url().should('not.include' , '/web/index.php/dashboard/index');
-        loginpage.getErrorMessage("Invalid credentials");
-       })
-
-       it('Enter empty email and password', () => {
-        loginpage.clickLogin();
-        cy.contains('Required').should('be.visible');
-        cy.url().should('not.include' , '/web/index.php/dashboard/index');
-       })
-
-
+  it("Login Test", () => {
+    loginpage.loginbuttonexist();
 });
 
+  it("login with valid credentials and logout", () => {
+    
+    loginpage.enteremailAddress(testdata.email);
+    loginpage.enterPassword(testdata.password);
+    loginpage.clickLogin();
+    cy.url().should('include', 'https://ecommerce-playground.lambdatest.io/index.php?route=account/account');
+    loginpage.user_logout();
+    cy.url().should('include', 'https://ecommerce-playground.lambdatest.io/index.php?route=account/logout');
+       
+  });
 
+  it('Enter wrong email and password', () => {
+    loginpage.enteremailAddress(testdata.invalid_un);
+    loginpage.enterPassword(testdata.invalid_psw);
+    loginpage.clickLogin();
+    cy.url().should('not.include' , '/web/index.php/dashboard/index');
+    loginpage.getErrorMessage();
+})
+
+  // it.skip("Enter wrong email and password", () => {
+  //   loginpage.enterUsername(testdata.invalid_username);
+  //   loginpage.enterPassword(testdata.invalid_password);
+  //   loginpage.clickLogin();
+  //   cy.url().should("not.include", "/web/index.php/dashboard/index");
+  //   loginpage.getErrorMessage("Invalid credentials");
+  // });
+
+  // it("Enter empty email and password", () => {
+  //   loginpage.clickLogin();
+  //   cy.contains("Required").should("be.visible");
+  //   cy.url().should("not.include", "/web/index.php/dashboard/index");
+  // });
+
+  // it("valid credentials", () => {
+  //   loginpage.enterUsername(testdata.username);
+  //   loginpage.enterPassword(testdata.password);
+  //   loginpage.clickLogin();
+  //   cy.url().should("include", "/web/index.php/dashboard/index");
+  //   loginpage.searchbar();
+  // });
+});
